6bc3c0994a62232f3a0e8e1fc31416129d229edb
javiermartinez27 <50684376+javiermartinez27@users.noreply.github.com>
2022-03-13 20:00:23 -0300

    prints

--- main.py
+++ main.py
@@ -709,10 +709,14 @@ def worker_sar():
         if temp[i] < 0:
             temp[i] = 0
     resp['SAR'] = list(temp)
-    print("VENTAS" + ventas)
-    print("LEN VENTAS" + len(ventas))
-    print("RESS" + ress[:22])
-    print("LEN RESS" + len(ress[:22]))
+    print("VENTAS")
+    print(ventas)
+    print("LEN VENTAS")
+    print(len(ventas))
+    print("RESS")
+    print(ress[:22])
+    print("LEN RESS")
+    print(len(ress[:22]))
     mse=mean_squared_error(ventas,ress[:22])
     msle=mean_squared_log_error(ventas,ress[:22])
     resp['msle']=msle
